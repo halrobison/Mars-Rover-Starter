c8237aa3d0aee68eae088b479ef9eab7500ded9f
halrobison <halrobisoncreative@gmail.com>
2024-02-20 21:25:59 -0600

    WIP task 4 - encountering resultsObject overwriting bug

--- rover.js
+++ rover.js
@@ -1,5 +1,75 @@
+const Message = require('./message.js');
+const Command = require('./command.js');
+
 class Rover {
-   // Write code here!
+   constructor(position){
+      this.position = position;
+      this.mode = 'NORMAL'; 
+      this.generatorWatts = 110; 
+   }
+   receiveMessage = function(message){
+      let returnObject = {
+         message: message.name,
+         results: commandResponse(message.commands)
+      }
+      return returnObject;
+   }
+}
+
+let commandResponse = function(commands){
+   let resultsArray = [];
+   let resultsObject = {
+      completed: null, 
+   }
+   for (i = 0; i < commands.length; i++){
+      if (commands[i].commandType === 'MOVE') {
+         if (this.mode === 'LOW_POWER') {
+            resultsObject.completed = false; 
+            resultsArray.push(resultsObject);
+         } else {
+            resultsObject.completed = true; 
+            this.position = commands[i].value;
+            resultsArray.push(resultsObject);
+         }
+      }
+      if (commands[i].commandType === 'STATUS_CHECK') {
+         resultsObject.completed = true;
+         let updatedResults = {
+            'mode': this.mode, 
+            'generatorWatts': this.generatorWatts, 
+            'position': this.position
+         }
+         resultsObject['roverStatus'] = updatedResults
+         resultsArray.push(resultsObject)
+      }
+      if (commands[i].commandType === 'MODE_CHANGE') {
+         if (commands[i].value = 'LOW_POWER') {
+            resultsObject.completed = true;
+            this.mode = 'LOW_POWER';
+         } else if (commands[i].value = 'LOW_POWER') {
+            resultsObject.completed = true; 
+            this.mode = 'NORMAL';
+         } else {
+            resultsObject.completed = false;
+         }
+         resultsArray.push(resultsObject)
+      }
+   }
+   return resultsArray;
 }
 
 module.exports = Rover;
+
+let roverTest = new Rover(100);
+let commands = [
+   new Command('MOVE', 4321),
+   new Command('STATUS_CHECK'),
+   new Command('MODE_CHANGE', 'LOW_POWER'),
+   new Command('MOVE', 3579),
+   new Command('STATUS_CHECK')
+];
+let message = new Message('TA power', commands);
+let response = roverTest.receiveMessage(message);
+
+console.log(response);
+console.log(JSON.stringify(response, null, 2));
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -9,5 +9,34 @@ const Command = require('../command.js');
 describe("Rover class", function() {
 
   // 7 tests here!
+  it("constructor sets position and default values for mode and generatorWatts", function () {
+    let roverTest = new Rover(98382);
+    expect(roverTest.mode).toBe('NORMAL')
+    expect(roverTest.generatorWatts).toBe(110);
+  })
+
+  it("response returned by receiveMessage includes two results if two commands are sent in the message", function(){
+    let commands = [new Command('MODE_CHANGE'), new Command('STATUS_CHECK')];
+    let message = new Message('Test message', commands);
+    let roverTest = new Rover(98382); 
+    let response = roverTest.receiveMessage(message); 
+    expect(response.message).toBe('Test message');
+  })
+
+  it("response returned by receiveMessage includes two results if two commands are sent in the message", function(){
+    let commands = [new Command('MODE_CHANGE'), new Command('STATUS_CHECK')];
+    let message = new Message('Test message', commands);
+    let roverTest = new Rover(98382); 
+    let response = roverTest.receiveMessage(message); 
+    expect(response.results).toHaveLength(2);
+  })
+
+  it("responds correctly to the status check command", function() {
+    let commands = [new Command('STATUS_CHECK')]
+    let message = new Message('Status check', commands);
+    let roverTest = new Rover(98382); 
+    let response = roverTest.receiveMessage(message); 
+    expect(response.results).toBe([{completed: true, roverStatus: 'Status good'}])
+  })
 
 });
